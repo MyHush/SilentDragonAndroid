5bb293eb2a4294f254ddba247f63252fea02af13
jahway603 <jahway603@protonmail.com>
2020-06-26 19:36:13 -0400

    Added SilentDragon animated gif splash screen

---
+++ app/src/main/java/org/myhush/silentdragon/SplashActivity.kt
@@ -0,0 +1,28 @@
+// Copyright 2019-2020 The Hush developers
+// Released under the GPLv3
+package org.myhush.silentdragon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    // This is the loading time of the splash screen
+    private val SPLASH_TIME_OUT: Long = 3000    // 3 seconds
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            // This method will be executed once the timer is over
+            startActivity(Intent(this,MainActivity::class.java))
+
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+
+}
